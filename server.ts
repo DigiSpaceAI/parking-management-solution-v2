26d15bd82d51471bd7b552f1ce243fd8d7fa8ff6
DigiSpaceAI <digisolutions.fm@gmail.com>
2026-08-30 16:56:32 +0530

    Add files via upload

--- server.ts
+++ server.ts
@@ -110,7 +110,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-user-email, x-user-role');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   }
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
